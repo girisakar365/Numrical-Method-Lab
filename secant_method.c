4754c12b494d8f8d751382f6f1b811d1ddae832f
girisakar365 <sakargiri365@gmail.com>
2024-12-20 18:37:38 +0545

    secant method

---
+++ secant_method.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+#define f(x)  x*x*x - 4*x + 1;
+
+void main()
+{
+	 float x0, x1, x2, f0, f1, f2, e;
+	 int step = 1, N;
+	 printf("\nEnter initial guesses:\n");
+	 scanf("%f%f", &x0, &x1);
+	 printf("Enter tolerable error:\n");
+	 scanf("%f", &e);
+	 printf("Enter maximum iteration:\n");
+	 scanf("%d", &N);
+	
+	 printf("\nStep\t\tx1\t\tx2\t\tf(x1)\t\tf(x2)\t\tx3\n");
+	 do
+	 {
+		  f0 = f(x0);
+		  f1 = f(x1);
+		  if(f0 == f1)
+		  {
+			   printf("Mathematical Error.");
+			   exit(0);
+		  }
+		  
+		  x2 = x1 - (x1 - x0) * f1/(f1-f0);
+		  f2 = f(x2);
+		  
+		  printf("%d\t\t%f\t%f\t%f\t%f\t%f\n",step,x0,x1,f0,f1,x2);
+		  
+		  x0 = x1;
+		  f0 = f1;
+		  x1 = x2;
+		  f1 = f2;
+		  
+		  step = step + 1;
+		  
+		  if(step > N)
+		  {
+			   printf("Not Convergent.");
+			   exit(0);
+		  }
+	 }while(fabs(f2)>e);
+	
+	 printf("\nRoot is: %f", x2);
+}
